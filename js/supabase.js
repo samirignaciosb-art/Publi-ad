a73921cf9446cb3df087133b1dfc4cdb2d44f512
samirignaciosb-art <samirignaciosb@gmail.com>
2026-05-15 03:45:15 -0400

    Update Supabase anonymous key in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://hwkyvxzbheegxynoljrw.supabase.co'
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3a3l2eHpiaGVlZ3h5bm9sanJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2MDMxOTgsImV4cCI6MjAyODE3OTE5OH0.uP_-_DPeWRGRjkGxRfmVhYsKMJqHBt8JcfMcJXaUTGo'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3a3l2eHpiaGVlZ3h5bm9sanJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzEyNzYsImV4cCI6MjA4NzU0NzI3Nn0.izGO-iK49rb-Rx5GRl2Rm55iDNj10sjCCq2QVEfuSXg'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
   auth: {
