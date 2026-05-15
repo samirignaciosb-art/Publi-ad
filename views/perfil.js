2d3d98eba53d11225d3bbd305e00078292d0e676
samirignaciosb-art <samirignaciosb@gmail.com>
2026-05-15 02:40:24 -0400

    Add renderPerfil function for profile view

---
+++ views/perfil.js
@@ -0,0 +1,4 @@
+// js/views/perfil.js
+export async function renderPerfil(container) {
+  container.innerHTML = '<div class="view-placeholder"><div class="placeholder-icon">🐾</div><div class="placeholder-sub">Vista perfil — en construcción</div></div>'
+}
